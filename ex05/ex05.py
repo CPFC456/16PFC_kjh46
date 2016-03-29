18ff80e8a32b7229be64122e65e5d7c596cba93f
CAD Client <CAD Client>
2016-03-29 11:49:48 +0900

    ex05

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,18 +1,21 @@
-my_name = 'Jinhyeong Kim'
-my_age = 21 # not a lie
-my_height = 2 # meters
-my_weight = 180 # lbs
+name = 'Jinhyeong Kim'
+my_age = 21 # It's a lie
+my_height_cm = 180.5
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 80
 my_eyes = 'Black'
 my_teeth = 'Brown'
 my_hair = 'Black'
-
-print "Let's talk about %s" % my_name
-print "He's %d meters tall." % my_height
-print "He's %d pounds heavy." % my_weight
+# please hojder
+print "Let's talk about %s" % name
+print "He's %g centimeters tall." % my_height_cm
+print "He's %g inches tall" % my_height_inch
+print "He's %d kilograms heavy." % my_weight_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
  # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg)

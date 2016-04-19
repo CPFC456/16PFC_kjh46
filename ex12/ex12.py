1b7ffad905bebdf742d6457c0e903d78332e5475
Jinhyeong Kim <rlawlsgud46@naver.com>
2016-04-19 11:06:36 +0900

    ex12.py added

---
+++ ex12/ex12.py
@@ -0,0 +1,7 @@
+age = raw_input("How old are you?   21")
+height = raw_input("How tall are you?   200")
+weight = raw_input("How much do you weigh?   50")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    21, 200, 50)
+# what the

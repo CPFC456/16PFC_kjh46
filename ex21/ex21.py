7d471edb543930eb270ef5d96fc418628b917d3d
KJH <rlawlsgud46@naver.com>
2016-05-17 11:04:01 +0900

    ex21.py 커밋

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -1,19 +1,21 @@
+# coding: utf-8
+
 def add(a, b):
     print ("ADDING %d + %d" % (a, b))
     return a + b
-
+# 더하기에 대한 함수
 def subtract(a, b):
     print ("SUBTRACTING %d - %d" % (a, b))
     return a - b
-
+# 빼기에 대한 함수
 def multiply(a, b):
     print ("MULTIPLYING %d * %d" % (a, b))
     return a * b
-
+# 곱하기에 대한 함수
 def divide(a, b):
     print ("DIVIDING %d / %d" % (a, b))
     return a / b
-
+# 나누기에 대한 함수
 
 print ("Let's do some math with just functions!")
 
@@ -21,7 +23,7 @@ age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
-
+# 변수를 입력
 print ("Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
 
 
